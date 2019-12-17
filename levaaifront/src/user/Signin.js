1c7e698325a2be1a304b027260c04ab83565f1b8
Bruno De A. Bisogni <bruno@levoo.com.br>
2019-12-17 15:56:49 -0300

    estrutura erro em carregar texto

---
+++ levaaifront/src/user/Signin.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Signin = () => <div>Faça seu login</div>; 
+
+export default Signin; 
---
+++ levaaifront/src/user/Signup.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Signup = () => <div>Formulário de Cadastro</div>; 
+
+export default Signup; 
